c9e4f23cf465bd3ab3ddfcc53dbcaf0e168c1a21
Michael Bumann <hello@michaelbumann.com>
2020-05-05 22:08:35 +0200

    v0.10.0 with support for routerrpc

--- lib/lnrpc.rb
+++ lib/lnrpc.rb
@@ -1,10 +1,22 @@
-require "lnrpc/version"
-require "lnrpc/rpc_services_pb"
+require 'lnrpc/version'
+require 'lnrpc/rpc_services_pb'
+require 'lnrpc/router_services_pb'
+require 'securerandom'
 
 module Lnrpc
   class Error < StandardError; end
   autoload :Client, 'lnrpc/client'
+  autoload :GrpcWrapper, 'lnrpc/grpc_wrapper'
   autoload :MacaroonInterceptor, 'lnrpc/macaroon_interceptor'
+
+  PREIMAGE_BYTE_LENGTH = 32
+  KEY_SEND_PREIMAGE_TYPE = 5482373484
+
+  def self.create_preimage
+    SecureRandom.random_bytes(PREIMAGE_BYTE_LENGTH)
+  end
+
+  def self.to_byte_array(str)
+    [str].pack("H*")
+  end
 end
-
-
--- lib/lnrpc/client.rb
+++ lib/lnrpc/client.rb
@@ -3,31 +3,11 @@ require "lnrpc/macaroon_interceptor"
 module Lnrpc
   class Client
     attr_accessor :address, :credentials, :macaroon
-    attr_writer :grpc_client
 
     LND_HOME_DIR = ENV['LND_HOME'] || "~/.lnd"
     DEFAULT_ADDRESS = 'localhost:10009'
     DEFAULT_CREDENTIALS_PATH = "#{LND_HOME_DIR}/tls.cert"
     DEFAULT_MACAROON_PATH = "#{LND_HOME_DIR}/data/chain/bitcoin/mainnet/admin.macaroon"
-
-    NON_CONVENTION_REQUEST_CLASSES = {
-      add_invoice: Lnrpc::Invoice,
-      send_payment: Lnrpc::SendRequest,
-      send_payment_sync: Lnrpc::SendRequest,
-      open_channel_sync: Lnrpc::OpenChannelRequest,
-      send_to_route_sync: Lnrpc::SendToRouteRequest,
-      lookup_invoice: Lnrpc::PaymentHash,
-      decode_pay_req: Lnrpc::PayReqString,
-      describe_graph: Lnrpc::ChannelGraphRequest,
-      get_chan_info: Lnrpc::ChanInfoRequest,
-      get_node_info: Lnrpc::NodeInfoRequest,
-      get_network_info: Lnrpc::NetworkInfoRequest,
-      stop_daemon: Lnrpc::StopRequest,
-      update_channel_policy: Lnrpc::PolicyUpdateResponse,
-      subscribe_channel_graph: Lnrpc::GraphTopologySubscription,
-      subscribe_invoices: Lnrpc::InvoiceSubscription,
-      subscribe_transactions: Lnrpc::GetTransactionsRequest
-    }
 
     def initialize(options={})
       self.address = options[:address] || DEFAULT_ADDRESS
@@ -46,42 +26,36 @@ module Lnrpc
       self.macaroon = options[:macaroon]
     end
 
-    def grpc_client
-      @grpc_client ||= Lnrpc::Lightning::Stub.new(self.address,
-                                                    GRPC::Core::ChannelCredentials.new(self.credentials),
-                                                    interceptors: [Lnrpc::MacaroonInterceptor.new(self.macaroon)]
-                                                  )
+    def lightning
+      @lightning ||= GrpcWrapper.new(Lnrpc::Lightning::Stub.new(address,
+                                                    GRPC::Core::ChannelCredentials.new(credentials),
+                                                    interceptors: [Lnrpc::MacaroonInterceptor.new(macaroon)]
+                                                  ))
     end
 
-    def pay(payreq)
-      self.send_payment_sync(Lnrpc::SendRequest.new(payment_request: payreq))
+    def router
+      @router ||= GrpcWrapper.new(Routerrpc::Router::Stub.new(address,
+                                                    GRPC::Core::ChannelCredentials.new(credentials),
+                                                    interceptors: [Lnrpc::MacaroonInterceptor.new(macaroon)]
+                                                  ))
     end
 
-    def method_missing(m, *args, &block)
-      if self.grpc_client.respond_to?(m)
-        params  = args[0]
+    def keysend(args)
+      args[:dest_custom_records] ||= {}
+      args[:dest_custom_records][Lnrpc::KEY_SEND_PREIMAGE_TYPE] ||= Lnrpc.create_preimage
+      args[:payment_hash] ||= Digest::SHA256.digest(args[:dest_custom_records][Lnrpc::KEY_SEND_PREIMAGE_TYPE])
+      args[:timeout_seconds] ||= 60
+      router.send_payment_v2(args)
+    end
 
-        args[0] = params.nil? ? request_class_for(m).new : request_class_for(m).new(params)
-        self.grpc_client.send(m, *args, &block)
-      else
-        super
-      end
+    def pay(args)
+      args[:timeout_seconds] ||= 60
+      router.send_payment_v2(args)
     end
 
     def inspect
       "#{self.to_s} @address=\"#{self.address}\""
     end
 
-    private
-    def request_class_for(method_name)
-      if NON_CONVENTION_REQUEST_CLASSES.key?(method_name.to_sym)
-        NON_CONVENTION_REQUEST_CLASSES[method_name.to_sym]
-      else
-        klass = method_name.to_s.sub(/^[a-z\d]*/) { |match| match.capitalize }
-        klass.gsub!(/(?:_|(\/))([a-z\d]*)/i) { "#{$1}#{$2.capitalize}" }
-        Lnrpc.const_get("#{klass}Request")
-      end
-    end
   end
 end
-
