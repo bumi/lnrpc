b42d640767d9d9f0497dd71a13360dd275547713
Michael Bumann <hello@michaelbumann.com>
2020-08-29 16:38:51 +0200

    Update examples.rb

--- examples.rb
+++ examples.rb
@@ -11,8 +11,15 @@ pay_request = "lntb50u1pw9mmndpp5nvnff958pxc9eqknwntyxapjw7l5grt5e2y70cmmnu0lljf
 lnd.pay(pay_request) # or:
 lnd.router.send_payment_v2(payment_request: pay_request)
 
+dest = Lnrpc.to_byte_array('038474ec195f497cf4036e5994bd820dd365bb0aaa7fb42bd9b536913a1a2dcc9e')
+lnd.keysend(dest: dest, amt: 1000)
+
 invoice_res = lnd.lightning.add_invoice(value: 1000, memo: 'I :heart: ruby')
 puts invoice_res.payment_request
+
+puts lnd.versioner.get_version
+puts lnd.wallet_kit.estimate_fee(conf_target: 10)
+puts lnd.wallet_kit.next_addr
 
 lnd.lightning.subscribe_invoices(settle_index: 1).each do |invoice|
   puts invoice.payment_request
