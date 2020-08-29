2e903a25bda534274307a429ea3792f10cc4d506
Michael Bumann <hello@michaelbumann.com>
2020-08-29 16:52:51 +0200

    Update examples.rb

--- examples.rb
+++ examples.rb
@@ -8,7 +8,7 @@ puts get_info_res.alias
 puts lnd.lightning.wallet_balance.total_balance
 
 pay_request = "lntb50u1pw9mmndpp5nvnff958pxc9eqknwntyxapjw7l5grt5e2y70cmmnu0lljfa0sdqdpsgfkx7cmtwd68yetpd5s9xct5v4kxc6t5v5s8yatz0ysxwetdcqzysxqyz5vqjkhlyn40z76gyn7dx32p9j58dftve9xrlvnqqazht7w2fdauukhyhr9y4k3ngjn8s6srglj8swk7tm70ng54wdkq47ahytpwffvaeusp500csz"
-lnd.pay(pay_request) # or:
+lnd.pay(payment_request: pay_request) # or:
 lnd.router.send_payment_v2(payment_request: pay_request)
 
 dest = Lnrpc.to_byte_array('038474ec195f497cf4036e5994bd820dd365bb0aaa7fb42bd9b536913a1a2dcc9e')
